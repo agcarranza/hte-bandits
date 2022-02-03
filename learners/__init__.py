0eea1a23711642f4ffb4db7d60a02a7fe9cca9bf
Aldo Gael Carranza <gael@DN0a22b125.SUNet>
2022-02-02 17:16:44 -0800

    Add init file

---
+++ learners/__init__.py
@@ -0,0 +1,7 @@
+from .learners import IGWBanditLearner, SemiparametricIGWBanditLearner, EpsilonGreedyLearner
+
+# __all__ = [
+#     'IGWBanditLearner',
+#     'SemiparametricIGWBanditLearner',
+#     'EpsilonGreedyLearner'
+# ]
